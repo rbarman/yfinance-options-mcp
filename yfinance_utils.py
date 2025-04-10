eef162045f76cf5cfee5ee965d4b04bd087bf93a
Rohan Barman <rohan1barman@gmail.com>
2025-04-10 00:35:34 -0500

    .

--- yfinance_utils.py
+++ yfinance_utils.py
@@ -6,20 +6,6 @@ class OptionType(Enum):
     PUT = "PUT"
     CALL = "CALL"
 
-# TODO: add expiration date to the options chain, by default yfinance returns the closest expiration date
-# ticker.option_chain(date=expiration_date)
-
-
-# async def _get_put_options_chain(ticker_symbol: str) -> pd.DataFrame:
-#     ticker = yf.Ticker(ticker_symbol)
-#     option_chain = ticker.option_chain().puts
-#     return option_chain
-
-# async def _get_call_options_chain(ticker_symbol: str) -> pd.DataFrame:
-#     ticker = yf.Ticker(ticker_symbol)
-#     option_chain = ticker.option_chain().calls
-#     return option_chain
-
 def format_options_chain(options_chain: pd.DataFrame) -> str:
     # keep only the columns that are needed
     df =  options_chain[["strike", "lastPrice", "change", "percentChange", "volume", "openInterest", "impliedVolatility", "inTheMoney"]]
